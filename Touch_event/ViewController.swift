465025aa1bdb813603a5e2998e443ac73f8e9375
Rayne <lukain337@gmail.com>
2016-01-13 20:46:06 +0900

    initial commit

---
+++ Touch_event/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Touch_event
+//
+//  Created by Rayne on 1/13/16.
+//  Copyright © 2016 Kappa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var imageNameArray: [String] = ["hana", "hoshi", "onpu", "shitumon"]
+    
+    var imageIndex: Int = 0
+    
+    @IBOutlet var haikeiImageView: UIImageView!
+    
+    var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func selectedFirst() {
+        
+    }
+    @IBAction func selectedSecond() {
+        
+    }
+    @IBAction func selectedThird() {
+        
+    }
+    @IBAction func selectedFourth() {
+        
+    }
+
+
+}
+
+
